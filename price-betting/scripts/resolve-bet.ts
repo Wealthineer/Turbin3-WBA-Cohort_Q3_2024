1864e2d156164d2f09f6c2d808b62ab8f13f8866
wealthineer <hi@wealthineer.com>
2024-09-16 19:47:02 +0200

    restructuring

---
+++ price-betting/scripts/resolve-bet.ts
@@ -0,0 +1,112 @@
+import { Keypair, SystemProgram, PublicKey, Connection, Commitment} from "@solana/web3.js";
+import adminWallet from "./wallets/admin.json";
+import takerWallet from "./wallets/taker.json";
+import makerWallet from "./wallets/maker.json";
+import { AnchorProvider, Program, Wallet } from "@coral-xyz/anchor";
+import { confirmTx } from "./utils";
+import { PriceBetting, IDL } from "./programs/price_betting";
+
+import {BN, workspace, web3, getProvider} from "@coral-xyz/anchor";
+import { AnchorUtils, asV0Tx, loadLookupTables, PullFeed } from "@switchboard-xyz/on-demand";
+
+export const TX_CONFIG = {
+    commitment: "processed" as Commitment,
+    skipPreflight: true,
+    maxRetries: 0,
+  };
+
+async function main() {
+
+    const admin = Keypair.fromSecretKey(new Uint8Array(adminWallet));
+    const betTaker = Keypair.fromSecretKey(new Uint8Array(takerWallet));
+    const betCreator = Keypair.fromSecretKey(new Uint8Array(makerWallet));
+
+    console.log("Admin", admin.publicKey.toBase58());
+    console.log("Bet Taker", betTaker.publicKey.toBase58());
+    console.log("Bet Creator", betCreator.publicKey.toBase58());
+
+    const connection = new Connection("https://api.devnet.solana.com", 'confirmed');
+
+    const provider = new AnchorProvider(connection, new Wallet(admin), { commitment: "confirmed" });
+    
+    const program = new Program<PriceBetting>(IDL, provider);
+
+    const initSeed = new BN(123);
+    const betSeed = new BN(999);
+    const openUntil = new BN(Date.now() + 1000 * 60);
+    const resolveDate = new BN(Date.now() + 1000 * 60 * 3);
+    const pricePrediction = new BN(1000);
+    const amount = new BN(1 * web3.LAMPORTS_PER_SOL);
+    const fees = 1000;
+    const bonkUsdSwitchboardFeedDevnet = "2N5FN6TiH6hVroPkt4zoXHPEsDHp6B8cSV38ALnJic46";    
+
+
+    const betProgram  = PublicKey.findProgramAddressSync([Buffer.from("program"), admin.publicKey.toBuffer(), initSeed.toArrayLike(Buffer, "le", 8)], program.programId)[0];
+    console.log("Bet Program", betProgram.toBase58());
+    const treasury  = PublicKey.findProgramAddressSync([Buffer.from("treasury"), betProgram.toBuffer()], program.programId)[0];
+    console.log("Treasury", treasury.toBase58());
+    const bet  = PublicKey.findProgramAddressSync([Buffer.from("bet"), betProgram.toBuffer(), betCreator.publicKey.toBuffer(), betSeed.toArrayLike(Buffer, "le", 8)], program.programId)[0];
+    console.log("Bet", bet.toBase58());
+    const bettingPool = PublicKey.findProgramAddressSync([Buffer.from("betting_pool"), bet.toBuffer()], program.programId)[0];
+    console.log("Betting Pool", bettingPool.toBase58());
+
+    //@ts-ignore
+    const unresolvedBet = await program.account.bet.fetch(bet)
+
+    console.log("Unresolved Bet: ")
+    console.log(unresolvedBet)
+
+
+    const feed = new PublicKey(bonkUsdSwitchboardFeedDevnet);
+
+    const switchboardProgram = await AnchorUtils.loadProgramFromEnv();
+
+    //@ts-ignore
+    const feedAccount = new PullFeed(switchboardProgram, feed);
+    const [pullIx, responses, _ , luts] = await feedAccount.fetchUpdateIx();
+
+    console.log("Responses: ", responses)
+    console.log("Luts: ", luts)
+
+    const lookupTables = await loadLookupTables([...responses.map((x) => x.oracle), feedAccount]);
+
+    console.log("Lookup Tables: ", lookupTables)
+
+    //@ts-ignore
+    const resolveIx = await program.methods.resolveBetWihtoutUpdate(betSeed).accountsPartial({
+        resolver: betCreator.publicKey,
+        betCreator: betCreator.publicKey,
+        betProgram: betProgram,
+        bet: bet,
+        resolverFeed: new PublicKey(bonkUsdSwitchboardFeedDevnet),
+      }).instruction();
+
+    const tx = await asV0Tx({
+        connection,
+        ixs: [pullIx, resolveIx],
+        signers: [betCreator],
+        computeUnitPrice: 200_000,
+        computeUnitLimitMultiple: 1.3,
+        // lookupTables: lookupTables,
+      });
+
+    // simulate the transaction
+    const simulateResult =
+    await connection.simulateTransaction(tx, {
+        commitment: "processed",
+    });
+    console.log(simulateResult);
+
+    // Send the transaction via rpc 
+    const sig = await connection.sendTransaction(tx);
+
+    await confirmTx(provider, sig)
+
+    //@ts-ignore
+    const resolvedBet = await program.account.bet.fetch(bet)
+
+    console.log("Resolved Bet: ")
+    console.log(resolvedBet)
+}
+
+main();
